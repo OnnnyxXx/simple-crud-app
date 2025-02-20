ddf9a1efe31bdaca60711b03ff142be3e603ace4
OnnnyxXx <oooivanlau2@gmail.com>
2025-02-20 14:01:58 +0300

    Updated: UserRepository

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -11,6 +10,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "select * from users where email = :email", nativeQuery = true)
     Optional<User> findByEmail(String email);
 
+    @Query(value = "select * from users where name= :name", nativeQuery = true)
     Optional<User> findByName(String name);
 
 }
