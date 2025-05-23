ecc299d1110afded5f6c306b8e35f11bfc8b083d
OnnnyxXx <oooivanlau2@gmail.com>
2025-05-23 18:55:38 +0300

    feat: update PatchMapping

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,10 +2,16 @@ package com.example.demo.controller;
 
 import com.example.demo.repository.User;
 import com.example.demo.repository.UserDto;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -14,8 +20,16 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    private final UserRepository userRepository;
+
+    private final ObjectMapper objectMapper;
+
+    public UserController(UserService userService,
+                          UserRepository userRepository,
+                          ObjectMapper objectMapper) {
         this.userService = userService;
+        this.userRepository = userRepository;
+        this.objectMapper = objectMapper;
     }
 
     @PostMapping("/create")
@@ -38,13 +52,9 @@ public class UserController {
         return userService.findByEmail(email);
     }
 
-    @PutMapping("/update/{id}")
-    public void update(@PathVariable Long id,
-                       @RequestParam(required = false) String email,
-                       @RequestParam(required = false) String name) {
-
-        userService.update(id, email, name);
-
+    @PatchMapping("/update/{id}")
+    public User patch(@PathVariable Long id, @RequestBody JsonNode patchNode) throws IOException {
+        return userService.update(id, patchNode);
     }
 
     @DeleteMapping("/delete/{id}")
